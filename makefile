bf51c25a5fbd56811b57202f193525f647f7b8ee
LucasNeiaTorres <LucasNeiaTorres>
2024-12-12 18:52:06 -0300

    sem mpi

--- makefile
+++ makefile
@@ -1,22 +1,43 @@
-flags = -Wall
-name = multi_partition_mpi
+# flags = -Wall
+# name = multi_partition_mpi
 
-all: $(name)
+# all: $(name)
 
-$(name): multi_partition_mpi.o chrono.o verifica_particoes.o
-	mpicc -o $(name) multi_partition_mpi.o chrono.o verifica_particoes.o $(flags)
+# $(name): multi_partition_mpi.o chrono.o verifica_particoes.o
+# 	mpicc -o $(name) multi_partition_mpi.o chrono.o verifica_particoes.o $(flags)
 
-multi_partition_mpi.o: multi_partition_mpi.c
-	mpicc -c multi_partition_mpi.c $(flags)
+# multi_partition_mpi.o: multi_partition_mpi.c
+# 	mpicc -c multi_partition_mpi.c $(flags)
 
-chrono.o: chrono.c
-	mpicc -c chrono.c $(flags)
+# chrono.o: chrono.c
+# 	mpicc -c chrono.c $(flags)
 
-verifica_particoes.o: verifica_particoes.c
-	mpicc -c verifica_particoes.c $(flags)
+# verifica_particoes.o: verifica_particoes.c
+# 	mpicc -c verifica_particoes.c $(flags)
+
+# clean:
+# 	rm -f *~ *.o
+
+# purge: clean
+# 	rm -f $(name)
+
+arquivos=multi_partition_sem.o verifica_particoes.o
+all: multi_partition_sem verifica_particoes
+
+multi_partition_sem: multi_partition_sem.o verifica_particoes.o chrono.o
+	gcc -o multi_partition_sem multi_partition_sem.o verifica_particoes.o chrono.o $(parametrosCompilacao)
+
+verifica_particoes.o: verifica_particoes.h verifica_particoes.c
+	gcc -c verifica_particoes.c $(parametrosCompilacao)
+
+chrono.o: chrono.h chrono.c
+	gcc -c chrono.c $(parametrosCompilacao)
+
+.o: .c
+	gcc -c *.c $(parametrosCompilacao)
 
 clean:
-	rm -f *~ *.o
+	rm -f *.o *.gch multi_partition_sem verifica_particoes chrono
 
 purge: clean
-	rm -f $(name)
+	-rm -f multi_partition_sem verifica_particoes chrono
--- verifica_particoes.c
+++ verifica_particoes.c
@@ -2,26 +2,54 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
-#include "mpi.h"
+// #include "mpi.h"
 
-void verifica_particoes( long long *Input, int n, long long *P, int np, long long *Output, int *nO) {
-    int processId;
-    MPI_Comm_rank(MPI_COMM_WORLD, &processId);
+// void verifica_particoes( long long *Input, int n, long long *P, int np, long long *Output, int *nO) {
+//     int processId;
+//     MPI_Comm_rank(MPI_COMM_WORLD, &processId);
 
-    int isCorrect = 1;
+//     int isCorrect = 1;
 
-    for (int i = 0; i < *nO; i++) {
-        if (Output[i] < (processId == 0 ? 0 : P[processId-1])
-            || Output[i] >= P[processId]) {
-            isCorrect = 0;
-            break;
+//     for (int i = 0; i < *nO; i++) {
+//         if (Output[i] < (processId == 0 ? 0 : P[processId-1])
+//             || Output[i] >= P[processId]) {
+//             isCorrect = 0;
+//             break;
+//         }
+//     }
+
+//     // Exibir resultado final
+//     if (isCorrect) {
+//         printf("===> particionamento CORRETO\n");
+//     } else {
+//         printf("===> particionamento COM ERROS no processo %d\n", processId);
+//     }
+// }
+
+void verifica_particoes(long long *Input, int n, long long *P, int np, long long *Output, int *Pos) {
+    int erro = 0;
+    long long faixa_min = 0;
+    long long faixa_max;
+
+    // Verificar as partições uma a uma
+    for (int i = 0; i < np; i++) {
+        faixa_max = P[i];
+        
+        for (int j = Pos[i]; j < ((i == np - 1) ? n : Pos[i + 1]); j++) {
+            if (Output[j] < faixa_min || Output[j] >= faixa_max) {
+                erro = 1;
+                // printf("Erro na faixa %d: elemento %lld fora do intervalo [%lld, %lld)\n", 
+                    //    i, Output[j], faixa_min, faixa_max);
+            }
         }
+
+        faixa_min = faixa_max;
     }
 
     // Exibir resultado final
-    if (isCorrect) {
-        printf("===> particionamento CORRETO\n");
+    if (erro) {
+        printf("===> particionamento COM ERROS\n");
     } else {
-        printf("===> particionamento COM ERROS no processo %d\n", processId);
+        printf("===> particionamento CORRETO\n");
     }
 }
